b7ea3245889fbb86a3c0961e7417192f7c53397a
Maria Helynne <mariahelynne@gmail.com>
2022-05-16 16:34:20 -0300

    solicita números do usuário e faz as operações aritméticas

--- aula.py
+++ aula.py
@@ -1,7 +1,10 @@
 print ("Hello World")
-a = 100
-b = 50
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
 print ("A soma dos números é", a+b)
 print ("A diferença dos números é", a-b)
 print ("O produto dos números é", a*b)
-print ("A divisão dos números é", a/b)
+if(b == 0):
+  print ("Não é possível dividir por zero")
+else:
+  print ("A divisão dos números é", a/b)
